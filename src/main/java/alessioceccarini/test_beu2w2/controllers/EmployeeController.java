ef495b4e7abef4dff5408e825965f602b21872e7
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-02-13 13:25:53 +0100

    metod post and get works

--- src/main/java/alessioceccarini/test_beu2w2/controllers/EmployeeController.java
+++ src/main/java/alessioceccarini/test_beu2w2/controllers/EmployeeController.java
@@ -2,13 +2,13 @@ package alessioceccarini.test_beu2w2.controllers;
 
 
 import alessioceccarini.test_beu2w2.entities.Employee;
+import alessioceccarini.test_beu2w2.payloads.EmployeeDTO;
 import alessioceccarini.test_beu2w2.services.EmployeesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("employees")
@@ -32,5 +32,9 @@ public class EmployeeController {
 
 	//------------------------------------ P O S T  ----------------------------------------------
 
-
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Employee addEmployee(@RequestBody @Validated EmployeeDTO employeeDTO) {
+		return this.employeesService.saveEmployee(employeeDTO);
+	}
 }
